30df5dcc3be5b5233abab2863bf3504578dfde50
cayenne17 <47927025+cayenne17@users.noreply.github.com>
2025-12-08 17:06:28 +0100

    Màj sh

--- Android/l_chasse.sh
+++ Android/l_chasse.sh
@@ -1,7 +1,8 @@
 #!/system/bin/sh
+##############
 #eNB Analytics :
 am start fr.enb_analytics.enb4g/.MainActivity
-
+sleep 1
 #am broadcast -a fr.enb_analytics.enb4g.SERVICE_START fr.enb_analytics.enb4g
 #am broadcast -a fr.enb_analytics.enb4g.SERVICE_STOP fr.enb_analytics.enb4g
 
@@ -10,24 +11,45 @@ am broadcast -a fr.enb_analytics.enb4g.RECORD_TRACES_ON fr.enb_analytics.enb4g
 
 am broadcast -a fr.enb_analytics.enb4g.RECORD_STUMBLER_ON fr.enb_analytics.enb4g
 #am broadcast -a fr.enb_analytics.enb4g.RECORD_STUMBLER_OFF fr.enb_analytics.enb4g
+#eNB Analytics
+##############
 
 
 
-#TowerCollector :
+##############
+#TowerCollector
 #am start info.zamojski.soft.towercollector/.SplashActivity
 am broadcast -a info.zamojski.soft.towercollector.COLLECTOR_START info.zamojski.soft.towercollector
 #am broadcast -a info.zamojski.soft.towercollector.COLLECTOR_STOP info.zamojski.soft.towercollector
+#TowerCollector
+##############
 
-#CellMapper :
+##############
+#CellMapper
 am start cellmapper.net.cellmapper/.MainActivity
+sleep 1
 am start -a startMapping cellmapper.net.cellmapper/.MainActivity
 #am start -a stopMapping cellmapper.net.cellmapper/.MainActivity
+#CellMapper
+##############
 
-#NSG :
+##############
+#NeoStumbler
+am start xyz.malkki.neostumbler.fdroid/xyz.malkki.neostumbler.MainActivity
+#sleep 1
+#am broadcast -a yz.malkki.neostumbler.xxxxxxxxxx yz.malkki.neostumbler
+#NeoStumbler
+##############
+
+##############
+#NSG
 am start com.qtrun.QuickTest/com.qtrun.nsg.LauncherActivity
+#NSG
+##############
 
-
-#nPerf :
+##############
+#nPerf
 am start com.nperf.tester/.Activity.SplashScreenActivity
+#nPerf
+##############
 
-exit
