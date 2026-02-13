1fada09073cb3554e735aff442dfe35d29191633
cayenne17 <47927025+cayenne17@users.noreply.github.com>
2026-02-13 18:07:22 +0100

    update tools_scripts/Android/l_chasse.sh

--- Android/l_chasse.sh
+++ Android/l_chasse.sh
@@ -1,4 +1,16 @@
 #!/system/bin/sh
+script_path="$(realpath "$0")"
+DIR_script="$(dirname "${script_path}")"
+
+##############
+#rename Orange F if Mobile O
+gsm_operator_alpha="$(getprop gsm.operator.alpha)"
+if [ "$(echo "${gsm_operator_alpha}" | grep -c "Mobile O")" -eq 1 ]; then
+	/system/bin/sh "${DIR_script}/Orange_F.sh"
+fi
+#RNCMobile if Free
+##############
+
 ##############
 #eNB Analytics :
 am start fr.enb_analytics.enb4g/.MainActivity
@@ -53,3 +65,15 @@ am start com.nperf.tester/.Activity.SplashScreenActivity
 #nPerf
 ##############
 
+
+##############
+#RNCMobile if Free
+if [ "$(echo "${gsm_operator_alpha}" | grep -c "Free")" -eq 1 ]; then
+	am start org.rncteam.rncfreemobile/.MainActivity
+	sleep 5
+	am broadcast -a org.rncteam.rncfreemobile.CELL_RECORDER_START org.rncteam.rncfreemobile
+fi
+#am broadcast -a org.rncteam.rncfreemobile.CELL_RECORDER_START org.rncteam.rncfreemobile
+#am broadcast -a org.rncteam.rncfreemobile.CELL_RECORDER_STOP org.rncteam.rncfreemobile
+#RNCMobile if Free
+##############
